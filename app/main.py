32ce81c12747dfce1ae1a7fe5d23aba89074a647
YuriyNik156 <yuriy.nik156@gmail.com>
2025-11-15 12:30:46 +0300

    Update main.py

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.api.endpoints import router
+from app.models import Base
+from app.deps import engine
+
+app = FastAPI(
+    title="Zakupki Parser API",
+    description="API для получения и сохранения данных о закупках",
+)
+
+# Создаём таблицы
+Base.metadata.create_all(bind=engine)
+
+# Подключаем эндпоинты
+app.include_router(router)
